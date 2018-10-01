07d6a32e75494252296be8ce631b106275275d70
kk <juergen@klotzek.de>
2018-10-01 11:34:53 +0200

    Tagessicherung

--- save_to_git.sh
+++ save_to_git.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+# cp db.sqlite3 db.sqlite_sicher  && \
 git add . && \
 git status && \
 git commit -m "Tagessicherung"  && \
-rm .git/hooks/pre-push  && \
-cp db.sqlite3 db.sqlite_sicher  && \
+# rm .git/hooks/pre-push  && \
 #git push git@github.com:juergen@klotzek.de/Supplier-Management-System.git 
-git push
+git push origin master
+# rm db.sqlite_sicher
